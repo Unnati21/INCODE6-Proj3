9428b2ae13bc8c12d8250a9b34bbbe0c00475c58
Unnati Parikh <unnatisuthar21@gmail.com>
2021-10-16 19:35:50 +1100

    Add routes for users

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const morgan = require('morgan')
 const data =  require('./data')
 const app = express()// invoke express in order to create an instance
 
@@ -8,6 +9,10 @@ const PORT = process.env.PORT || 3000
 // JSON and form parsing middleware
 app.use(express.json()) 
 app.use(express.urlencoded({extended: true}))
+
+// Loging Middleware
+app.use(morgan('dev'))
+
 
 //ROUTES
 //S-2- 2 : Create the first routes to return all the informatin-(WELCOME)
@@ -32,19 +37,32 @@ app.get('/users/:id', (req, res) =>{
     
     res.send(user)
 })
-//S-3(get  URL '/users/2/schedu -t of all schedules for use)
-//------------------------------//
+//S-3--(get  URL '/users/2/schedu of all schedules for use)(use of fillter to get the particular array)
+app.get('/users/:id/schedules', (req, res) => {
+  const schedules = data.schedules.filter(schedule => schedule.user_id === Number(req.params.id))
+   //TODO: Validate, If array has data, than send it ,otherwise ,show error
+  res.send(schedules)
+
+})
 
 //S-4 : Create routes to update (Create new user)
  app.post('/users',(req, res) => {
      // USing bcryptsjs
-      const password = req.body.password
+     const password = req.body.password
       const salt = bcrypt.genSaltSync(10)
       const hash = bcrypt.hashSync(password, salt)
+      data.users.push(req.body)
+    res.send(data.users)
 
       //ToDo: Add hash to user object and then push to user array
-      res.send(hash)
+     res.send(hash)
  })
+ //app.post('/users', (req, res) => {
+   
+   // data.users.push(req.body)
+    //res.send(data.users)
+
+ //})
  //creat to add a new schedule. It will return the newly created schdule
  app.post('/schedules',(req, res) => {
     //TODO: Validate Data
