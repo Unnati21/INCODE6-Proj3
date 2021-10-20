2283afb9e74aa07393c4d2222272ee45712e6b02
Unnati Parikh <unnatisuthar21@gmail.com>
2021-10-20 22:46:41 +1100

    add sql scripts and create schedule database

---
+++ sql/create_db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS mr_coffee_schedule;
+
+CREATE DATABASE mr_coffee_schedule;
---
+++ sql/seed_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+  schedules (username, day, start_at, end_at)
+  VALUES
+  ('Donald', '1', '7:00', '4:00'),
+  ('Ali', '2', '8:00', '6:00'),
+  ('Mikey', '4', '6:00', '4:00');
